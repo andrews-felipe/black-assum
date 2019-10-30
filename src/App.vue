5bc886fc089358f3273428594fefec031958a073
andrews-felipe <ferulip@gmail.com>
2019-10-30 14:31:44 -0300

    Adicionando métodos para gerar domínios, criando template modelo para redirecionar ao checkout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div id="header" class="text-center">
-      <h1>Celacanto</h1>
+      <h1>BlackAssum</h1>
       <h6 class="text-secondary">Gerador de StatupNames Vue.js Node.js GraphQL</h6>
     </div>
     <div id="main">
@@ -15,21 +15,20 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="prefix in prefixes"
-                    v-bind:key="prefix"
-                  >{{ prefix }}</li>
+                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+                    <div class="row">
+                      <div class="col">{{ prefix }}</div>
+                      <div class="col align-end">
+                        <button class="btn btn-info" type="submit">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
+                <br>
                 <div class="input-group">
-                  <input
-                    @keyup.enter="addPrefix(prefix)"
-                    class="form-control"
-                    type="text"
-                    name="prefixo"
-                    placeholder="Degite o Prefixo"
-                    v-model="prefix"
-                  />
+                  <input @keyup.enter="addPrefix(prefix)" class="form-control" type="text" placeholder="Digite o Prefixo" v-model="prefix"/>
                   <div class="input-group-append">
                     <button type="submit" class="btn btn-info" v-on:click="addPrefix(prefix)">
                       <span class="fa fa-plus"></span>
@@ -47,21 +46,20 @@
             <div class="card">
               <div class="card-body">
                 <ul class="list-group">
-                  <li
-                    class="list-group-item"
-                    v-for="sufix in sufixes"
-                    v-bind:key="sufix"
-                  >{{ sufix }}</li>
+                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+                    <div class="row">
+                      <div class="col">{{ sufix }}</div>
+                      <div class="col align-end">
+                        <button class="btn btn-info" type="submit">
+                          <span class="fa fa-trash"></span>
+                        </button>
+                      </div>
+                    </div>
+                  </li>
                 </ul>
+                <br>
                 <div class="input-group">
-                  <input
-                    @keyup.enter="addSufix(sufix)"
-                    class="form-control"
-                    type="text"
-                    name="prefixo"
-                    placeholder="Degite o Sufixo"
-                    v-model="sufix"
-                  />
+                  <input @keyup.enter="addSufix(sufix)" class="form-control" type="text" name="prefixo" placeholder="Degite o Sufixo" v-model="sufix"/>
                   <div class="input-group-append">
                     <button type="submit" class="btn btn-info" v-on:click="addSufix(sufix)">
                       <span class="fa fa-plus"></span>
@@ -81,11 +79,18 @@
           <div class="card">
             <div class="card-body">
               <ul class="list-group">
-                <li
-                  class="list-group-item"
-                  v-for="domain in domains"
-                  v-bind:key="domain"
-                >{{ domain }}</li>
+                <li class="list-group-item" v-for="domain in domains" v-bind:key="domain.name">
+                  <div class="row">
+                    <div class="col-md">
+                      {{ domain.name }}
+                    </div>
+                    <div class="col-md align-end">
+                      <a class="btn btn-info" :href="domain.url" target="_blank">
+                        <span class="fa fa-shopping-cart"></span>
+                      </a>
+                    </div>
+                  </div>
+                </li>
               </ul>
             </div>
           </div>
@@ -104,38 +109,49 @@ export default {
 
   data: function() {
     return {
-      //Variables
       prefix: "",
       sufix: "",
-      prefixes: ["Air", "Get", "Fligh1"],
+      prefixes: ["Air", "Get", "Flight"],
       sufixes: ["Orbe", "Flut", "Bower"],
-      domains: ["AirFlut", "GetBower", "FlightOrbe"]
     };
   },
   methods: {
     addPrefix(prefix) {
       this.prefixes.push(prefix);
       this.prefix = "";
-      this.gen();
     },
     addSufix(sufix) {
       this.sufixes.push(sufix);
       this.sufix = "";
-      this.gen();
     },
-    gen() {
-      this.domains = [];
+    deleteSufix(sufix){
+      this.sufixes.splice(this.sufixes.indexOf(sufix), 1)
+    }
+  },
+  computed:{
+    domains() {
+      console.log("Gerando Domínios...");
+      const domains = [];
       for (const prefix of this.prefixes) {
         for (const sufix of this.sufixes) {
-          this.domains.push(`${prefix} ${sufix}`);
+          const name = prefix + sufix
+          domains.push({
+            url : `https://checkout.hostgator.com.br/?a=add&sld=${name.toLowerCase()}&tld=.com`,
+            name
+          });
         }
       }
+      return domains;
     }
   }
 };
 </script>
 
 <style>
+.align-end{
+  display: flex;
+  justify-content: flex-end;
+}
 #header {
   margin: 30px;
 }
